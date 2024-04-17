af9f78aaf70e0d25878679b73d19c9afb9eece65
Anitha <anithamca.k2008@gmail.com>
2024-04-17 21:59:01 +0200

    Save

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-const deleteDoneBtn = document.querySelector('#deleteDoneBtn');
-const sendBackBtn = document.querySelector('#sendBackBtn');
+const deleteDoneBtn = document.querySelector("#deleteDoneBtn");
+const sendBackBtn = document.querySelector("#sendBackBtn");
+const element = document.getElementById("textboxid");
+console.log(element);
+const button1 = document.getElementById("submitbuttonid");
+const todos = [];
 
+button1.addEventListener("click", (mylist) => {
+  if (element.value === "") {
+    alert("Give some value in the TextBox");
+  } else {
+    // todos.forEach((task,index) => {
+    const li = document.createElement("li");
+    li.innerHTML = element.value;
+    console.log(element.value);
+    orderlistid.appendChild(li);
+
+    const newTodo = {
+      task: textboxid.value,
+    };
+    todos.push(newTodo);
+    console.log(newTodo);
+    console.log(todos);
+
+    localStorage.setItem("todolist", JSON.stringify(todos));
+  }
+
+  element.value = "";
+});
